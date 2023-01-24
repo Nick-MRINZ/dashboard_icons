1f4d9d50c8d9570853f1786603a48385ee5dd140
Nick-MRINZ <41651987+Nick-MRINZ@users.noreply.github.com>
2023-01-25 09:33:11 +1300

    Update dashboard.php
    Updated java script

--- dashboard.php
+++ dashboard.php
@@ -28,33 +28,35 @@ class dashboard extends AbstractExternalModule
 
 
           $('img').each(function() {
-    				if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_gray.png') {
-                $(this).replaceWith('<i class="far fa-circle"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_red.png') {
-                $(this).replaceWith('<i class="fas fa-times-circle text-danger"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_yellow.png'){
-              $(this).replaceWith('<i class="fas fa-question-circle text-warning"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_orange_tick.png'){
-              $(this).replaceWith('<i class="far fa-times-circle text-warning"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_green.png'){
-              $(this).replaceWith('<i class="fas fa-check-circle text-success"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_green_tick.png'){
-              $(this).replaceWith('<i class="far fa-check-circle text-success"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_red_stack.png'){
-              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-danger"></i><i class="fas fa-times fa-fw text-danger"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_yellow_stack.png'){
-              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-warning"></i><i class="fas fa-question fa-fw text-warning"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_green_stack.png'){
-              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-success"></i><i class="fas fa-check fa-fw text-success"></i>');
-            } else if($(this).attr('src') == '<?php echo APP_PATH_WEBROOT; ?>Resources/images/circle_blue_stack.png'){
-              $(this).replaceWith('<i class="fas fa-layer-group fa-fw"></i>');
+    				if($(this).attr('alt') == 'Incomplete (no data saved)') {
+                $(this).replaceWith('<i class="far fa-circle" alt=""></i>');
+            } else if($(this).attr('alt') == 'Incomplete') {
+                $(this).replaceWith('<i class="fas fa-times-circle text-danger" alt="Incomplete"></i>');
+            } else if($(this).attr('alt') == 'Unverified'){
+              $(this).replaceWith('<i class="fas fa-question-circle text-warning" alt="Unverified"></i>');
+            } else if($(this).attr('alt') == 'Partial Survey Response'){
+              $(this).replaceWith('<i class="far fa-times-circle text-warning" alt="Partial Survey Response"></i>');
+            } else if($(this).attr('alt') == 'Complete'){
+              $(this).replaceWith('<i class="fas fa-check-circle text-success" alt="Complete"></i>');
+            } else if($(this).attr('alt') == 'Completed Survey Response'){
+              $(this).replaceWith('<i class="far fa-check-circle text-success" alt="Completed Survey Response"></i>');
+            } else if($(this).attr('alt') == 'Many Statuses (Incomplete)'){
+              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-danger" alt="Many Statuses (Incomplete)"></i><i class="fas fa-times fa-fw text-danger"></i>');
+            } else if($(this).attr('alt') == 'Many Statuses (Unverified)'){
+              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-warning" alt="Many Statuses (Unverified)"></i><i class="fas fa-question fa-fw text-warning"></i>');
+            } else if($(this).attr('alt') == 'Many Statuses (Complete)'){
+              $(this).replaceWith('<i class="fas fa-layer-group fa-fw text-success" alt="Many Statuses (Complete)"></i><i class="fas fa-check fa-fw text-success"></i>');
+            } else if($(this).attr('alt') == 'Many Statuses (Mixed)'){
+              $(this).replaceWith('<i class="fas fa-layer-group fa-fw" alt="Many Statuses (Mixed)"></i>');
             }
           });
 
           <?php if(PAGE == 'DataEntry/record_status_dashboard.php' || PAGE == 'DataEntry/record_home.php'){ ?>
             $('#status-icon-legend').find("tr:gt(1)").remove();
-            $('#status-icon-legend').append('<tr><td class="nowrap" style="padding-right:5px;"><i class="fas fa-question-circle text-warning"></i> Unverified</td><td class="nowrap" style="padding-right:5px;"><i class="far fa-times-circle text-warning"></i> Partial Survey Response</td></tr>');
-            $('#status-icon-legend').append('<tr><td class="nowrap" style="padding-right:5px;"><i class="fas fa-check-circle text-success"></i> Complete </td><td class="nowrap" style="padding-right:5px;"><i class="far fa-check-circle text-success"></i> Completed Survey Response</td></tr>')
+            $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="fas fa-question-circle text-warning"></i> Unverified</td></tr>');
+            $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="fas fa-check-circle text-success"></i> Complete </td></tr>');
+            $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="far fa-times-circle text-warning"></i> Partial Survey Response</td></tr>');
+            $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="far fa-check-circle text-success"></i> Completed Survey Response</td></tr>');
             $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="fas fa-layer-group fa-fw"></i> Many Statuses (Mixed)</td></tr>');
             $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="fas fa-layer-group fa-fw text-success"></i><i class="fas fa-check fa-fw text-success"></i> Many Statuses (Complete)</td></tr>');
             $('#status-icon-legend').append('<tr><td colspan=2 class="nowrap" style="padding-right:5px;"><i class="fas fa-layer-group fa-fw text-warning"></i><i class="fas fa-question fa-fw text-warning"></i> Many Statuses (Unverified)</td></tr>');
